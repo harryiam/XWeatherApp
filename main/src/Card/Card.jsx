2085b65f98b005d6843fb9e52f13e0021be4eefb
AMBATI HARITHA <ambatiharitha@AMBATIs-MacBook-Air.local>
2024-03-10 16:14:51 +0530

    weather app

--- main/src/Card/Card.jsx
+++ main/src/Card/Card.jsx
@@ -1,23 +1,23 @@
 import React from "react"
-import styles from "./Card.module.css"
+import "./Card.module.css"
 
 export default function Card({temp,hum,con,windsp}){
     return(
         <>
-        <div className={styles["weather-cards"]}>
-        <div className={styles["weather-card"]}>
+        <div className="weather-cards">
+        <div className="weather-card">
             <h3>Temperature</h3>
             <h5>{temp}</h5>
         </div>
-        <div className={styles["weather-card"]}>
+        <div className="weather-card">
             <h3>Humidity</h3>
             <h5>{hum}</h5>
         </div>
-        <div className={styles["weather-card"]}>
+        <div className="weather-card">
             <h3>Condition</h3>
             <h5>{con}</h5>
         </div>
-        <div className={styles["weather-card"]}>
+        <div className="weather-card">
             <h3>Windspeed</h3>
             <h5>{windsp}</h5>
         </div>
